5d8f6b9deef3179839e499352da623c98d5f9c51
Streltsov Valerii <st.valera04@gmail.com>
2023-02-27 15:39:13 +0200

    Done first exersise

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const BODY = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+stopButton.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerInterval = null;
+
+function generateNewColorForBody() {
+  BODY.style.backgroundColor = getRandomHexColor();
+}
+
+function chainingBodyColors() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  timerInterval = setInterval(generateNewColorForBody, 1000);
+}
+
+function stopChangingBodyColors() {
+  clearInterval(timerInterval);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+startButton.addEventListener('click', chainingBodyColors);
+
+stopButton.addEventListener('click', stopChangingBodyColors);
